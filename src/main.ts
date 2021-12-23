0f9277025e1ed979607056e45b0cdd5ad85dd4b5
AlwaysLee <ddoddo1997@naver.com>
2021-12-23 18:06:46 +0900

    Initialize Project

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(3000);
+  console.log(
+    `============================listening on port ${port}=================================`,
+  );
+}
+bootstrap();
