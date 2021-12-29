254124d5ec4f156b227d8c5ac4a499acf77cb0c5
AlwaysLee <ddoddo1997@naver.com>
2021-12-29 16:44:09 +0900

    Feat: add beforeexit event listener

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  const prismaService: PrismaService = app.get(PrismaService);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -20,6 +22,8 @@ async function bootstrap() {
   console.log(
     `============================listening on port ${port}=================================`,
   );
+
+  prismaService.enableShutdownHooks(app); //Prisma는 NestJS의 enableShutdownHooks를 방해하기에 종료 신호를 수신하고 어플리케이션 종료 hook이 실행되기 전 process.exit()를 호출해야한다(이 코드는 해당 역할을 수행하는 코드).
 
   if (module.hot) {
     module.hot.accept();
