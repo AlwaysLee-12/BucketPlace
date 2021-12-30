c9960b73a6d310536c07e64e221ebf049a985e9f
AlwaysLee <ddoddo1997@naver.com>
2021-12-30 13:39:07 +0900

    Feat: add prisma logging

--- src/modules/users/users.resolver.ts
+++ src/modules/users/users.resolver.ts
@@ -20,6 +20,6 @@ export class UserResolver {
 
   @Query((returns) => User, { name: 'user' }) //name: 쿼리 이름, description: 설명, deprecationReason: 쿼리 지원 중단 설정, nullable: 쿼리가 null 데이터 응답을 반환할 수 있는지
   async getUser(@Args('id', { type: () => Int }) id: number) {
-    return this.userService.findOneById(id);
+    return this.userService.getUser(id);
   }
 }
--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -9,12 +9,25 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'stdout', level: 'info' },
+        { emit: 'stdout', level: 'warn' },
+        { emit: 'stdout', level: 'error' },
+      ],
+      errorFormat: 'colorless',
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
 
   async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
+    this.$on('beforeExit', async (e) => {
+      console.log(e.name);
       await app.close();
     });
   }
