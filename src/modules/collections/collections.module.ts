961a0578f6f6d9c78867fe2021d0df5971dcef04
AlwaysLee <ddoddo1997@naver.com>
2022-01-03 18:21:52 +0900

    Feat: resolver 각 모듈에 종속성 주입

--- src/modules/collections/collections.module.ts
+++ src/modules/collections/collections.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/modules/prisma/prisma.module';
 import { CollectionsController } from './collections.controller';
+import { CollectionResolver } from './collections.resolver';
 import { CollectionsService } from './collections.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [CollectionsController],
-  providers: [CollectionsService],
+  providers: [CollectionsService, CollectionResolver],
 })
 export class CollectionsModule {}
--- src/modules/places/places.module.ts
+++ src/modules/places/places.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/modules/prisma/prisma.module';
 import { PlacesController } from './places.controller';
+import { PlaceResolver } from './places.resolver';
 import { PlacesService } from './places.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [PlacesController],
-  providers: [PlacesService],
+  providers: [PlacesService, PlaceResolver],
 })
 export class PlacesModule {}
