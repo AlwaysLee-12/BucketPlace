816df80798d53dcb57f820ee11f3da791851c0aa
AlwaysLee <ddoddo1997@naver.com>
2022-01-03 17:51:11 +0900

    Feat: 각 feature 모듈에 prisma 모듈 종속성 주입

--- src/modules/collections/collections.module.ts
+++ src/modules/collections/collections.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/modules/prisma/prisma.module';
 import { CollectionsController } from './collections.controller';
 import { CollectionsService } from './collections.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [CollectionsController],
-  providers: [CollectionsService]
+  providers: [CollectionsService],
 })
 export class CollectionsModule {}
--- src/modules/places/places.module.ts
+++ src/modules/places/places.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/modules/prisma/prisma.module';
 import { PlacesController } from './places.controller';
 import { PlacesService } from './places.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PlacesController],
-  providers: [PlacesService]
+  providers: [PlacesService],
 })
 export class PlacesModule {}
