3afc8ed669fac0917e5548454cb4124cb6b8f896
AlwaysLee <ddoddo1997@naver.com>
2022-01-04 14:13:14 +0900

    Feat: gql playground 작동하게 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     GraphQLModule.forRoot({
       debug: false,
-      playground: false, //쿼리문을 던져 응답값을 확인할 수 있는 graphql IDE
+      playground: true, //쿼리문을 던져 응답값을 확인할 수 있는 graphql IDE
       include: [], //inclue 안에 있는 모듈만 스캔(default는 전체 앱에서 스캔)
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'), //코드 우선 접근 방식(TypeScript 클래스를 이용해 gql 스키마 생성)을 사용하기 위한 속성. 스키마가 생성될 경로
       //스키마를 메모리에서 바로 생성하려면, autoSchemaFile: true로
