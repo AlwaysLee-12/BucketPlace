b1b8c4af5308ef00bb2338b204556a7e0bbbc6d6
AlwaysLee <ddoddo1997@naver.com>
2022-01-13 17:01:16 +0900

    Feat: place resolver 요청 관련 로깅 구현

--- src/places/places.resolver.ts
+++ src/places/places.resolver.ts
@@ -7,6 +7,7 @@ import {
   Parent,
   Mutation,
 } from '@nestjs/graphql';
+import { Logger } from 'src/common/providers/logger.service';
 import { GetPlacesArgs } from './dto/get-places.args';
 import { PostPlaceArgs } from './dto/post-place.args';
 import { PlaceModel } from './models/place.model';
@@ -14,25 +15,34 @@ import { PlacesService } from './places.service';
 
 @Resolver(() => PlaceModel)
 export class PlaceResolver {
-  constructor(private readonly placeService: PlacesService) {}
+  constructor(
+    private readonly placeService: PlacesService,
+    private readonly logger: Logger,
+  ) {
+    this.logger.setContext(PlaceResolver.name);
+  }
 
   @Query(() => PlaceModel, { name: 'place' })
   async getPlace(@Args('id', { type: () => String }) id: string) {
+    this.logger.log('Get Place By ID');
     return this.placeService.getPlace(id);
   }
 
   @Query(() => [PlaceModel], { name: 'places' })
   async getPlaces(@Args() args: GetPlacesArgs) {
+    this.logger.log('Get All Place');
     return this.placeService.getPlaces(args);
   }
 
   @Mutation(() => PlaceModel, { name: 'postPlace' })
   async postPlace(@Args() args: PostPlaceArgs) {
+    this.logger.log('Post Place');
     return this.placeService.postPlace(args);
   }
 
   @Mutation(() => Boolean, { name: 'deletePlace' })
   async deletePlace(@Args('id') id: string) {
+    this.logger.log('Delete Posted Place');
     return this.placeService.deletePlace(id);
   }
 }
--- src/places/places.service.ts
+++ src/places/places.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Logger } from 'src/common/providers/logger.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { GetPlacesArgs } from './dto/get-places.args';
 import { PostPlaceArgs } from './dto/post-place.args';
@@ -7,9 +8,13 @@ import { PlaceModel } from './models/place.model';
 
 @Injectable()
 export class PlacesService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private readonly logger: Logger) {
+    this.logger.setContext(PlacesService.name);
+  }
 
   async getPlace(placeId: string): Promise<PlaceModel | null> {
+    this.logger.log('Get Place By ID Service');
+
     return await this.prisma.place.findUnique({
       where: { id: placeId },
       include: {
@@ -19,12 +24,16 @@ export class PlacesService {
   }
 
   async getPlaces(args: GetPlacesArgs): Promise<PlaceModel[]> {
+    this.logger.log('Get All Place Service');
+
     const { skip, take } = args;
 
     return await this.prisma.place.findMany({ skip, take });
   }
 
   async postPlace(args: PostPlaceArgs): Promise<PlaceModel> {
+    this.logger.log('Post Place Service');
+
     const { userId, postData } = args;
     const { name, location } = postData;
 
@@ -38,6 +47,8 @@ export class PlacesService {
   }
 
   async deletePlace(placeId: string): Promise<boolean> {
+    this.logger.log('Delete Place Service');
+
     try {
       const deleteCollectionPlaceQuery = this.prisma.collectionPlace.deleteMany(
         {
