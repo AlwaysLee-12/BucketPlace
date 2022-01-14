372318bdc09238072e93ab2c753a954d9a670774
AlwaysLee <ddoddo1997@naver.com>
2022-01-14 13:50:49 +0900

    Style: 불필요한 코드 삭제

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,6 +1,4 @@
 import { Global, Module } from '@nestjs/common';
-import { Logger } from './providers/logger.service';
-import { RequestContext } from './providers/request-context.service';
 import * as providers from './providers';
 
 @Global()
